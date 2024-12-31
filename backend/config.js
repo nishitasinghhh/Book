0b79dc0cda52af34b56fc1986f56f092177a2f5e
NishitaPrachi <chuckinmymemories@gmail.com>
2024-12-31 09:07:33 +0530

    first commit

---
+++ backend/config.js
@@ -0,0 +1,2 @@
+export const PORT=5555
+export const mongoDBURL='mongodb+srv://nishitasingh022:2XxRRT1Qn1g7XJmg@mern-book-store.ys2kf.mongodb.net/?retryWrites=true&w=majority&appName=Mern-book-store'
---
+++ backend/index.js
@@ -0,0 +1,27 @@
+import express from "express"
+import { PORT ,mongoDBURL} from "./config.js"
+import mongoose from "mongoose"
+import { Book } from "./models/bookModel.js"
+import booksRoute from './routes/booksRoute.js'
+import cors from 'cors'
+const app=express()
+app.use(express.json())
+app.use(cors())
+//app.use(cors({
+  //  origin:'http://localhost:5173',
+    //methods:['GET','POST','PUT','DELETE'],
+   //allowedHeaders:['Content-Type']
+//}))
+app.get('/' ,(request,response)=>{
+console.log(request)
+return response.status(234).send('Hey welcomd')
+})
+app.use('/books',booksRoute)
+mongoose.connect(mongoDBURL).then((result) => {
+    console.log('App connected to database')
+    app.listen(PORT,()=>{
+        console.log(`App is listening to ${PORT}`)
+    })
+}).catch((err) => {
+    console.log(err)
+});
