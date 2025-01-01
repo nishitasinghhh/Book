31ab650d827611d88b816b268a6430d190c17339
NishitaPrachi <chuckinmymemories@gmail.com>
2025-01-01 10:00:43 +0530

    new changes

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,15 @@
 import express from "express"
-import { PORT ,mongoDBURL} from "./config.js"
+
 import mongoose from "mongoose"
 import { Book } from "./models/bookModel.js"
 import booksRoute from './routes/booksRoute.js'
+import path from "path"
 import cors from 'cors'
+
+import {PORT,mongoDBURL} from './config.js'
+
 const app=express()
+const _dirname=path.resolve()
 app.use(express.json())
 app.use(cors())
 //app.use(cors({
@@ -12,11 +17,16 @@ app.use(cors())
     //methods:['GET','POST','PUT','DELETE'],
    //allowedHeaders:['Content-Type']
 //}))
-app.get('/' ,(request,response)=>{
-console.log(request)
-return response.status(234).send('Hey welcomd')
-})
+
 app.use('/books',booksRoute)
+app.use(express.static(path.join(_dirname,"/frontend/dist")))
+app.get('*',(_,res)=>{
+    res.sendFile(path.resolve(_dirname,"frontend","dist","index.html"))
+})
+app.get('/' ,(request,response)=>{
+    console.log(request)
+    return response.status(234).send('Hey welcome')
+    })
 mongoose.connect(mongoDBURL).then((result) => {
     console.log('App connected to database')
     app.listen(PORT,()=>{
